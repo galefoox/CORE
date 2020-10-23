5e01a8919ed0d3bb3d68ff392ef7d7d121da7469
galefoox <foox.gal.e@gmail.com>
2020-10-22 22:30:56 -0400

    Progress - moving point - drone path

--- potato2.py
+++ potato2.py
@@ -110,12 +110,12 @@ ax.scatter(xs, ys, zs, c=np.linalg.norm([xs, ys, zs], axis=0))
 
 # draw cube
 # formula in email (-a,b,c) ...
-corners = [(-3, 4, 3), (3, 4, 3),  (-3, -4, 3), (3, -4, 3),
-           (-3, 4, -3), (3, 4, -3), (-3, -4, -3), (3, -4, -3)]
-x = [p[0] for p in corners]
-y = [p[1] for p in corners]
-z = [p[2] for p in corners]
-ax.scatter(x, y, z, color="r")
+# corners = [(-3, 4, 3), (3, 4, 3),  (-3, -4, 3), (3, -4, 3),
+#            (-3, 4, -3), (3, 4, -3), (-3, -4, -3), (3, -4, -3)]
+# x = [p[0] for p in corners]
+# y = [p[1] for p in corners]
+# z = [p[2] for p in corners]
+# ax.scatter(x, y, z, color="r")
 
 
 # blue blob for spacecraft ADD IMAGE LATER
@@ -125,6 +125,64 @@ z = 5
 plt.plot(x, y, z, 'bo')
 
 
+# ===========================================================================================
+# create the parametric curve
+t = np.arange(0, 2*np.pi, 2*np.pi/100)  # list points
+x = 4 * np.cos(t)  # list of all x so cos(t) at all times
+y = 4 * np.sin(t)
+z = t/(2.*np.pi)
+
+x[0] = 5
+y[0] = 5
+z[0] = 5
+
+point, = ax.plot([x[0]], [y[0]], [z[0]], 'o')
+line, = ax.plot(x, y, z, label='parametric curve')
+
+
+def update_point(n, x, y, z, point):
+    point.set_data(np.array([x[n], y[n]]))
+    point.set_3d_properties(z[n], 'z')
+    return point
+
+
+ani = animation.FuncAnimation(fig, update_point, 99, fargs=(x, y, z, point))
+# ========================================================================================
+# =========================/////////////////////////////////////////================================
+# q = [[5.00, 4.91, -0.27, -4.07, -0.65, -1.95],
+#      [5.00, 0.71,  4.05, 5.66, -4.49,  -5.46], [5.00, 5.14, 2.80, 1.75, 6.24, 4.0267]]
+# v = [[0.0068, 0.024, -0.014, -0.013, -0.0068, -0.04], [0.012,
+#                                                        0.056, -0.022, 0.016,  0.0045, 0.039],
+#      [-0.0045,  0.031,  0.077, 0.0016, -0.015, -0.00012]]
+
+# x = np.array(q[0])
+# y = np.array(q[1])
+# z = np.array(q[2])
+# s = np.array(v[0])
+# u = np.array(v[1])
+# w = np.array(v[2])
+
+# point, = ax.plot(x, y, z, '*')
+
+
+# def update_points(t, x, y, z, points):
+
+#     new_x = x + s * t
+#     new_y = y + u * t
+#     new_z = z + w * t
+#     print('t:', t)
+
+#     # update properties
+#     point.set_data(new_x, new_y)
+#     point.set_3d_properties(new_z, 'z')
+
+#     # return modified artists
+#     return point
+
+
+# ani = animation.FuncAnimation(
+#     fig, update_points, frames=30, fargs=(x, y, z, point))
+# =========================/////////////////////////////////////////================================
 plt.show()
 
 # - close file
